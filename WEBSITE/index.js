ab2b087ea4350f635143f31f5350eabfaba156a4
JohnIanOngayi <ongayijohnian@gmail.com>
2024-02-27 17:13:06 +0300

    Type Conversion Basics

--- WEBSITE/index.js
+++ WEBSITE/index.js
@@ -1,18 +1,15 @@
-//window.alert(`This is an alert`);
-//window.alert(`Stop It Now!`);
-//"Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.";
-let age = 26;
-let name = "Dimmz";
-////document.getElementById("main_header").textContent = name;
-////document.getElementById("lorem").textContent = name;
-let online = true;
+/*TYPE CONVERSIONS*/
+let age = window.prompt("How old are you");
+console.log(`${typeof age}`);
+//Use Number Function to convert to integer
+age = Number(age);
+console.log(`${typeof age}`);
 console.log(`${age}`);
-console.log(typeof age);
-console.log(`First name ${name} is of type ${typeof name}`);
-console.log(`Variable ${online} is of type ${typeof online}`);
-console.log(`${age * 2}`);
-document.getElementById("submit_usr_name").onclick = function () {
-  let username = document.getElementById("fetch").value;
-  document.getElementById("main_header").textContent = `Hello ${username}`;
-  console.log(`Backtickling ${username}`);
-};
+/*
+ * Other type converter functions include
+ *  - String()
+ *  - Boolean()
+ *      As long as there's some value Boolean returns true
+ *      A typical usecase for this would be to check whether user
+ *      added input required
+ * */
