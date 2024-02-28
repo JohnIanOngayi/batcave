76f9ccd3f9d70682b18710822a701be64327914a
JohnIanOngayi <ongayijohnian@gmail.com>
2024-02-28 10:38:44 +0300

    Dice Roller
     Changes to be committed:
    	modified:   index.html
    	modified:   index.js

--- WEBSITE/index.js
+++ WEBSITE/index.js
@@ -1,25 +1,42 @@
 /*
- * A counter program
- * */
-let number = 0;
+ * THE Math OBJECT
+ *
+ * Does a bunch of math operations
+ */
+let a = 6;
+let b = 4;
+let c = 1;
+let d = 3.26;
+let e = -2.51;
+let z;
 
-document.getElementById("decr").onclick = function () {
-  number -= 1;
-  document.getElementById("digit_header").textContent = number;
-};
+let PI = Math.PI;
+let E = Math.E;
+z = Math.round(d);
+z = Math.floor(d);
+z = Math.ceil(d);
+z = Math.trunc(d);
+z = Math.pow(a, b);
+z = Math.sqrt(a);
+z = Math.log(d);
+z = Math.sin(c);
+z = Math.cos(d);
+z = Math.tan(d);
+z = Math.abs(d);
+z = Math.sign(e);
 
-document.getElementById("incr").onclick = function () {
-  number += 1;
-  document.getElementById("digit_header").textContent = number;
-};
-
-document.getElementById("reset").onclick = function () {
-  number = 0;
-  document.getElementById("digit_header").textContent = number;
+/*
+ * RANDOM NUMBER GENERATOR WEBSITE FOR ROLLING A SIX-SIDED DICE
+ *    using the Math object
+ */
+const generate = document.getElementById("generate");
+const number = document.getElementById("digit_header");
+generate.onclick = function () {
+  let myRandom = Math.floor(Math.random() * 6) + 1;
+  number.textContent = myRandom;
 };
 /*
- * what would have been easier would be fetching by id and storing in const's
- *  - const decr = document.getElementById("decr")
- *  - const incr = document.getElementById("incr")
- *  - const reset = document.getElementById("reset")
- * */
+ * The Math.random() returns a long decimal value between 0 and 1
+ * Say I want a range between max and min
+ * myRandomNum = Math.floor(Math.random() * (max - min)) + min
+ */
