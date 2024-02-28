3091ce90ca5af413e1a49d4312e14177f8081cc6
JohnIanOngayi <ongayijohnian@gmail.com>
2024-02-28 12:49:46 +0300

    Age Validator
     Changes to be committed:
    	modified:   index.html
    	modified:   index.js
    	modified:   styles.css

--- ALX-Backup/0x07-networking_basics/README.md
+++
@@ -1,3 +0,0 @@
-# 0x07-networking_basics
-
-An introductory repo folder to Computer Netwrking with Linux Systems
--- WEBSITE/index.js
+++ WEBSITE/index.js
@@ -1,42 +1,11 @@
 /*
- * THE Math OBJECT
- *
- * Does a bunch of math operations
+ * Using if statements to lock youngins out
  */
-let a = 6;
-let b = 4;
-let c = 1;
-let d = 3.26;
-let e = -2.51;
-let z;
-
-let PI = Math.PI;
-let E = Math.E;
-z = Math.round(d);
-z = Math.floor(d);
-z = Math.ceil(d);
-z = Math.trunc(d);
-z = Math.pow(a, b);
-z = Math.sqrt(a);
-z = Math.log(d);
-z = Math.sin(c);
-z = Math.cos(d);
-z = Math.tan(d);
-z = Math.abs(d);
-z = Math.sign(e);
-
-/*
- * RANDOM NUMBER GENERATOR WEBSITE FOR ROLLING A SIX-SIDED DICE
- *    using the Math object
- */
-const generate = document.getElementById("generate");
-const number = document.getElementById("digit_header");
-generate.onclick = function () {
-  let myRandom = Math.floor(Math.random() * 6) + 1;
-  number.textContent = myRandom;
+let age = document.getElementById("usrname_age").value;
+document.getElementById("btn_submit").onclick = function () {
+  if (Number(age) < 18) {
+    window.alert("access denied");
+  } else {
+    window.alert("Welcome");
+  }
 };
-/*
- * The Math.random() returns a long decimal value between 0 and 1
- * Say I want a range between max and min
- * myRandomNum = Math.floor(Math.random() * (max - min)) + min
- */
