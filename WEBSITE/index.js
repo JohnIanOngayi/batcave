23744f6d0ea90acce180db54cba3c356dd1d2a8e
JohnIanOngayi <ongayijohnian@gmail.com>
2024-02-28 09:51:03 +0300

    A Simple Counter Program
     Changes to be committed:
    	modified:   index.html
    	modified:   index.js
    	modified:   styles.css

--- WEBSITE/index.js
+++ WEBSITE/index.js
@@ -1,6 +1,25 @@
 /*
- * const - a variable that cannot be chaged
+ * A counter program
  * */
-const PI = 3.14159;
-let radius = Number(window.prompt("Enter a Radius"));
-console.log(`Circumference = ${2 * PI * radius}`);
+let number = 0;
+
+document.getElementById("decr").onclick = function () {
+  number -= 1;
+  document.getElementById("digit_header").textContent = number;
+};
+
+document.getElementById("incr").onclick = function () {
+  number += 1;
+  document.getElementById("digit_header").textContent = number;
+};
+
+document.getElementById("reset").onclick = function () {
+  number = 0;
+  document.getElementById("digit_header").textContent = number;
+};
+/*
+ * what would have been easier would be fetching by id and storing in const's
+ *  - const decr = document.getElementById("decr")
+ *  - const incr = document.getElementById("incr")
+ *  - const reset = document.getElementById("reset")
+ * */
