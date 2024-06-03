eb8a603dee8c3299d3c570f510f34106adb1724e
JohnIanOngayi <ongayijohnian@gmail.com>
2024-06-03 11:57:06 +0300

    MOre basic stuff for dummies

--- RestAPIPractice/main.py
+++ RestAPIPractice/main.py
@@ -7,9 +7,16 @@ app = Flask(__name__)
 api = Api(app)
 
 
+names = {
+    "tim": {"name": "tim", "age": 26, "gender": "male"},
+    "jane": {"name": "jane", "age": 23, "gender": "female"},
+    "ian": {"name": "ian", "age": 24, "gender": "male"}
+}
+
+
 class HelloWorld(Resource):
     def get(self, name="HelloWorld"):
-        return {"data": name}
+        return names[name]
 
     def post(self, name="Successful post"):
         return {"data": f"Successful post {name}"}
