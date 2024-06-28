7052873e18bab618f1fc817dd4ffec3c08b3e502
JohnIanOngayi <ongayijohnian@gmail.com>
2024-06-28 10:55:37 +0300

    Using fs

--- nodejs_play/play.js
+++ nodejs_play/play.js
@@ -1,15 +1,34 @@
 #!/usr/bin/node
 
 const fs = require("fs");
-fs.readFile("./docs/text.txt", (err, data) => {
-  if (err) {
-    console.log(err);
+
+if (!fs.existsSync("./deletables")) {
+  fs.mkdir("./deletables", () => {
+    console.log("Created Folder");
+  });
+
+  if (!fs.existsSync("./deletables/delete_me")) {
+    fs.writeFile("./deletables/delete_me", "Hey oh Dr!", () => {
+      console.log("Created and Wrote into delete_me!");
+    });
   } else {
-    console.log(data.toString());
+    console.log("delete_me already exists, we finna delete it!");
+    fs.unlink("./deletables/delete_me", (err) => {
+      if (err) {
+        console.log(`ERROR WHILE DELETING delete_me`);
+        console.log(err);
+      } else {
+        console.log("Successfully Deleted delete_me");
+      }
+    });
   }
-});
-console.log(`last line`);
-const data = "Hell Yeahh!!";
-fs.writeFile("./docs/write.md", data, () => {
-  console.log(`Booyah!`);
-});
+} else {
+  fs.rmdir("./deletables/", (err) => {
+    if (err) {
+      console.log(`ERROR WHILE DELETING FOLDER!`);
+      console.log(err);
+    } else {
+      console.log("Successfully Deleted deletables folder");
+    }
+  });
+}
