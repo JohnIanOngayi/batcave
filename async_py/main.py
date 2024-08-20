e40c6ef0d78f2f5a752732eb29c0ae10d40467d8
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-20 09:38:45 +0300

    chore(docs): added proper comments

--- async_py/main.py
+++ async_py/main.py
@@ -3,27 +3,38 @@
 import asyncio
 import time
 
+# Creating coroutine
 async def task_1():
     print('--x--starting  tasks--x--')
     print('     sending email 1')
-    asyncio.create_task(task_2())
+    # Pause exec, wait for this to finish
     await asyncio.sleep(2)
     print('     email1 response')
+    return 'a'
 
+# Creating coroutine
 async def task_2():
     print('     sending email 2')
-    asyncio.create_task(task_3())
     await asyncio.sleep(2)
     print('     email2 response')
+    return 'b'
 
+# Creating coroutine
 async def task_3():
     print('     sending email 3')
     await asyncio.sleep(2)
     print('     email3 response')
     print('--x--all emails sent--x--')
+    return 'c'
 
-start_time = time.time()
-asyncio.run(task_1())
-end_time = time.time()
-print(end_time - start_time)
+async def main():
+    start_time = time.time()
+    # Gathering/ Scheduling coroutines and then pausing main exec using await
+    # all return values fetched in tuple called results
+    results = await asyncio.gather(task_1(), task_2(), task_3())
+    end_time = time.time()
+    print('Time: ', end_time - start_time)
+    print('Results: ', results)
+
+asyncio.run(main())
 # print(asyncio.iscoroutinefunction(task_1))
