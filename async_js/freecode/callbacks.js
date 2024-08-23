ae86ac9b3234d8f71761c671b749c1b74753218e
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-23 10:09:07 +0300

    Simulated callback hell

--- async_js/freecode/callbacks.js
+++ async_js/freecode/callbacks.js
@@ -1,12 +1,65 @@
 #!/usr/bin/node
 
-function one(callback) {
-  console.log(" Step 1 ");
-  callback();
-}
+let stocks = {
+  fruits: ["strawberry", "grapes", "banana", "apple"],
+  liquids: ["water", "ice"],
+  holders: ["cone", "cup", "stick"],
+  toppings: ["chocolate", "peanuts"],
+};
 
-function two() {
-  console.log(" Step 2 ");
-}
+let order = (fruitname, call_production) => {
+  setTimeout(() => {
+    console.log(`Order for ${stocks.fruits[fruitname]} placed`);
+    call_production();
+  }, 2000);
+};
 
-one(two);
+let production = () => {
+  setTimeout(() => {
+    console.log("Production started");
+    cutting();
+  }, 0);
+};
+
+let cutting = () => {
+  setTimeout(() => {
+    console.log("Cutting fruit");
+    liquidate();
+  }, 2000);
+};
+
+let liquidate = () => {
+  setTimeout(() => {
+    console.log(`${stocks.liquids[0]} and ${stocks.liquids[1]} was added`);
+    start_machine();
+  }, 2000);
+};
+
+let start_machine = () => {
+  setTimeout(() => {
+    console.log("The machine was started");
+    select_container();
+  }, 1000);
+};
+
+let select_container = () => {
+  setTimeout(() => {
+    console.log(`Packaged in ${stocks.holders[1]}`);
+    add_toppings();
+  }, 2000);
+};
+
+let add_toppings = () => {
+  setTimeout(() => {
+    console.log(`${stocks.toppings[1]} was added`);
+    serve();
+  }, 2000);
+};
+
+let serve = () => {
+  setTimeout(() => {
+    console.log("Icecream is served");
+  }, 2000);
+};
+
+order(0, production);
