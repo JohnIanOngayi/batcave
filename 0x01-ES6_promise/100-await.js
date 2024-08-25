fb80488ae93c873fb9bf8faa406448a54e8bc546
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-25 20:01:54 +0300

    I ca't believe it

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+
+import { uploadPhoto, createUser } from "./utils";
+
+export default async function asyncUploadUser() {
+  let status = {};
+  try {
+    const photoStatus = await uploadPhoto();
+    const userStatus = await createUser();
+    status = {
+      photo: photoStatus,
+      user: userStatus,
+    };
+  } catch (err) {
+    status = {
+      photo: null,
+      user: null,
+    };
+  }
+  return status;
+}
