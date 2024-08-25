d4dafe9c1b74a15bb95cf0adaa290545dd786083
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-25 12:28:00 +0300

    Promises done and dusted

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,1 +1,6 @@
 #!/usr/bin/node
+
+export default function divideFunction(numerator, denominator) {
+  if (denominator !== 0) return numerator / denominator;
+  else throw new Error("Cannot divide by 0");
+}
