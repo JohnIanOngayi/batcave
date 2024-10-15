6555b8730915487f6a2a7d66b88f07391e723728
JohnIanOngayi <ongayijohnian@gmail.com>
2024-10-15 10:40:29 +0300

    Stream Pipes

--- nodejs_fs/streamIndex.js
+++ nodejs_fs/streamIndex.js
@@ -1,6 +1,7 @@
 import { fileURLToPath } from "url";
 import path, { dirname } from "path";
 import { createReadStream, createWriteStream } from "fs";
+import { unlink } from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,9 +13,17 @@ try {
   const ws = createWriteStream(path.join(__dirname, "files", "newLorem.txt"));
 
   /** Streamz more efficient */
-  rs.on("data", (dataChunk) => {
-    console.log(`dataChunk from rs: \n${dataChunk}`);
-    ws.write(dataChunk);
+  // rs.on("data", (dataChunk) => {
+  //   console.log(`dataChunk from rs: \n${dataChunk}`);
+  //   ws.write(dataChunk);
+  //   console.log("Finished writing");
+  // });
+
+  /** Pipe much more efficient */
+  unlink(path.join(__dirname, "files", "newLorem.txt"), (error) => {
+    if (error) throw error;
+    console.log("newLorem.txt deleted");
+    rs.pipe(ws);
     console.log("Finished writing");
   });
 } catch (error) {
