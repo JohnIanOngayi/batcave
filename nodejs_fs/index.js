84c066273e07105cbbb19f458415f64ed6051e74
JohnIanOngayi <ongayijohnian@gmail.com>
2024-10-15 09:24:25 +0300

    Writing to file asynchronously

--- nodejs_fs/index.js
+++ nodejs_fs/index.js
@@ -1,4 +1,4 @@
-import { readFile } from "fs";
+import { appendFile, readFile, writeFile } from "fs";
 import { fileURLToPath } from "url";
 import path, { dirname } from "path";
 
@@ -10,6 +10,25 @@ readFile(path.join(__dirname, "files", "starter.txt"), "utf-8", (err, data) => {
   if (err) throw err;
   console.log(data);
 });
+
+/* Writing to file asynchronously */
+writeFile(
+  path.join(__dirname, "files", "reply.txt"),
+  "text data for reply.txt",
+  (err) => {
+    if (err) throw err;
+    console.log("Write complete!");
+    /* Appending to file asynchronously */
+    appendFile(
+      path.join(__dirname, "files", "reply.txt"),
+      "text to append to reply.txt",
+      (err) => {
+        if (err) throw err;
+        console.log("Append complete!");
+      },
+    );
+  },
+);
 
 process.on("uncaughtException", (error) => {
   console.error(error);
